f8825bfec2ec2247336b11a52c08a962d64c0e8c
wangxian <wangxian1994@126.com>
2017-05-19 22:31:36 +0800

    delete build

--- build/CMakeFiles/PIRM.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PIRM.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PIRM.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/Drivers/CMakeFiles/STM32F4xx_StdPeriph_Driver.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libSTM32F4xx_StdPeriph_Driver.a"
-)
